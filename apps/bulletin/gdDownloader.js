5ff730d983b582347748497e4e0bfcc961016254
Gary Anderson <ganderson@seattleacademy.org>
2017-01-30 00:35:08 -0800

    add Google docs inport code

---
+++ apps/bulletin/gdDownloader.js
@@ -0,0 +1,22 @@
+var https = require('https');
+let docURL = "https://docs.google.com/document/export?format=txt&id=";
+
+module.exports = function (docId, cb) {
+    https.get(docURL + docId, function (res) {
+        const statusCode = res.statusCode;
+        if (statusCode !== 200) {
+            res.resume();
+            return cb(new Error('Request Failed. Status Code:' + statusCode));
+        }
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', function (chunk) {
+            rawData += chunk;
+        });
+        res.on('end', function () {
+            cb(null, rawData);
+        });
+    }).on('error', function (e) {
+        cb(e);
+    });
+};
--- apps/bulletin/index.js
+++ apps/bulletin/index.js
@@ -1,16 +1,21 @@
 var alexa = require('alexa-app');
+var gdDownloader = require('./gdDownloader');
 
+var docId = "1NWsJGeDsZ_uRsHKUJYRjbKB-gTLW-9gygEPNjmm6DHA";
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1; //When 0, you need to relaunch the server
 
 // Define an alexa-app
 var app = new alexa.app('bulletin');
 app.launch(function(req, res) {
-    function getArticle(err, result) {
-        console.log(result)
-        res.say(result).send();
-    }
-    setTimeout(function(){ getArticle(0,"hello"); }, 10);
+    gdDownloader(docId, function(err, txt) {
+        if (err) {
+            console.log('Error on getting text ', err);
+            return;
+        }
+        res.say(txt).send();
+        console.log(txt);
+    })
     return false;
 });
 app.intent('CommandIntent', {
